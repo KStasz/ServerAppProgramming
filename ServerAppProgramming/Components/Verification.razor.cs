2aebd216cf73d6084fdc57c7b144abf77596d517
KStasz <61710462+KStasz@users.noreply.github.com>
2020-06-11 20:57:11 +0200

    11.06.2020_3

---
+++ ServerAppProgramming/Components/Verification.razor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using ServerAppProgramming.Data;
+using ServerAppProgramming.Models;
+
+namespace ServerAppProgramming.Components
+{
+    public partial class Verification : ComponentBase
+    {
+        [Inject]
+        public ApplicationDbContext _dbContext { get; set; }
+
+        public List<SummaryOrder> listOrder { get; set; }
+
+        protected override void OnInitialized()
+        {
+            listOrder = ReadData();
+        }
+
+        private List<SummaryOrder> ReadData()
+        {
+            List<ServerAppProgramming.Models.Order> locallist = _dbContext.Order.Where(x => x.IsInWeryfication == true).ToList();
+            listOrder = new List<SummaryOrder>();
+            foreach (var item in locallist)
+            {
+                listOrder.Add(new SummaryOrder()
+                {
+                    Id = item.Id,
+                    Quantity = item.Quantity,
+                    meal = _dbContext.Meals.Where(x => x.Id == item.MealId).First(),
+                    restaurant = _dbContext.Restaurants.Where(x => x.Id == item.RestaurantId).First(),
+                    date = item.CreationDate,
+                    Username = _dbContext.Users.Where(x => x.Id == item.UserId).First().UserName
+                });
+            }
+            return listOrder;
+        }
+    }
+}
--- ServerAppProgramming/Models/SummaryOrder.cs
+++ ServerAppProgramming/Models/SummaryOrder.cs
@@ -8,6 +8,7 @@ namespace ServerAppProgramming.Models
         public Restaurants restaurant { get; set; }
         public Categories category { get; set; }
         public int Quantity { get; set; }
+        public string Username { get; set; }
         public bool IsInEdit { get; set; } = true;
         public bool IsInWeryfication { get; set; }
         public DateTime date { get; set; }
